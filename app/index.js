b52188d30ca69c04c530b6fc972aa78f3e699235
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-03-20 23:16:47 -0300

    expo-router: header components

--- app/index.js
+++ app/index.js
@@ -1,10 +1,51 @@
-import { View, Text } from 'react-native'
+// react
+import { useState } from 'react';
+import { SafeAreaView, ScrollView, View } from 'react-native';
+
+// expo router
+import { Stack, useRouter } from 'expo-router'
+
+// assets
+import { COLORS, FONT, icons, images, SHADOWS, SIZES } from '../constants'
+
+// components
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome
+} from '../components'
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen options={{
+        headerStyle: { backgroundColor: COLORS.lightWhite }, // header background
+        headerShadowVisible: false, // removing shadow
+        headerLeft: () => ( // element on the left at the header
+          <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+        ),
+        headerRight: () => ( // element on the right at the header
+          <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+        ),
+        headerTitle: "" // header title content
+      }} />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
