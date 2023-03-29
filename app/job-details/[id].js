f8082354e281c173fb950adf34ff4e018a10a0cd
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-03-28 21:08:40 -0300

    expo-router: company details in the job-details page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -77,7 +77,12 @@ const JobDetails = () => {
             <Text>No data</Text>
           ) : (
             <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-              <Company />
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
 
               <JobTabs />
             </View>
